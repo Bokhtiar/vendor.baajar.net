5027a7c210105e7007b8d6eff96a1fd8b3e0e3d2
getch-hasan <hasan15-3112@diu.edu.bd>
2025-05-19 12:53:27 +0600

    done

--- src/components/modal/productDelete.jsx
+++ src/components/modal/productDelete.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DeleteProductModal = ({ product, onDelete, onCancel }) => {
   return (
     <div  className="fixed inset-0 bg-black/80 bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white rounded-lg p-6 w-full max-w-xl shadow-lg text-center">
+      <div className="bg-white rounded-lg m-4 p-6 w-full max-w-xl shadow-lg text-center">
         <div className="flex flex-col items-center">
           <svg
             className="w-12 h-12 text-red-500 mb-4"
@@ -15,7 +15,7 @@ const DeleteProductModal = ({ product, onDelete, onCancel }) => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M1 7h22m-5 0V4a1 1 0 00-1-1H7a1 1 0 00-1 1v3" />
           </svg>
           <h2 className="text-[#8B8B8B] mb-4">The Following Product will be deleted</h2>
-          <div className="flex items-center font-medium justify-center gap-6 ">
+          <div className="flex md:flex-row flex-col items-center font-medium justify-center gap-6 ">
             <img src={product.image} alt={product.name} className="w-16 h-16 object-cover rounded" />
            
               <p>{product.name}</p>
